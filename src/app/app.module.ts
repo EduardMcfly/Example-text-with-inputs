8bbcdb47aa76001196f98624a95b96459f16657e
joyeria2018 <sipsajoyeria2018@gmail.com>
2019-02-06 10:42:08 -0500

    Se crea el primer commit, y se implementa las rutas de la aplicacion.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LayoutModule } from "@angular/cdk/layout";
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule
+} from "@angular/material";
+import { AppRoutingModule } from "./app-routing.module";
+import { MyDashboardComponent } from "./my-dashboard/my-dashboard.component";
+import { RatesComponent } from "./rates/rates.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MyDashboardComponent, RatesComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    AppRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
