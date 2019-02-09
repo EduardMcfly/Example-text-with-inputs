95f9005fd9f5d7a5af10cf5641d71c840c7f9c2a
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2019-02-09 15:02:20 -0500

    Se vinculan todos los modulos y declaraciones que va usar el modulo de parqueadero.

--- src/app/parking/parking.module.ts
+++ src/app/parking/parking.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
-import { EntriesComponent, ExitsComponent, DialogEntry, DialogExit } from "./";
+import {
+  EntriesComponent,
+  ExitsComponent,
+  DialogEntry,
+  DialogExit,
+  DialogCreateExit
+} from "./";
 import { ParkingComponent } from "./principal/parking.component";
 
 import { ParkingRoutingModule } from "./parking-routing.module";
 import { MaterialModule } from "../app.module.material";
+import { DialogConfirm } from "../components/dialog.confirm/dialog.confirm.component";
 
 @NgModule({
   imports: [CommonModule, FormsModule, ParkingRoutingModule, MaterialModule],
-  entryComponents: [DialogEntry, DialogExit],
+  entryComponents: [DialogEntry, DialogExit, DialogCreateExit, DialogConfirm],
   declarations: [EntriesComponent, ParkingComponent, ExitsComponent]
 })
 export class ParkingModule {}
