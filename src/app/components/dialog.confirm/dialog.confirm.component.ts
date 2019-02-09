08e5c57a87e30938a52563822472fc92c4d575ea
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2019-02-09 14:58:59 -0500

    Se crea un componente Universal para confirmar si un registro se desea borrar.

---
+++ src/app/components/dialog.confirm/dialog.confirm.component.ts
@@ -0,0 +1,38 @@
+import { Component, Inject, OnInit, Directive } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+
+import * as _ from "lodash";
+
+@Component({
+  selector: "dialog-confirm",
+  templateUrl: "dialog.confirm.component.html"
+})
+export class DialogConfirm implements OnInit {
+  entryForm: FormGroup;
+  submitted = false;
+  loading = false;
+  getData: Function;
+  openSnackBar: Function;
+  isNew: boolean;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogConfirm>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      isNew: boolean;
+      reloadData: Function;
+    }
+  ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {}
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.entryForm.controls;
+  }
+}
