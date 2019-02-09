76013c0a024a79ba6851161adc030dbf7840c527
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2019-02-09 14:55:45 -0500

    Se corrige un prublema con angular Router.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
 const navList: { title: string; path: string; icon?: string }[] = [
-  { path: "parking", title: "Parqueadero", icon: "local_parking" },
+  { path: "parking/entries", title: "Parqueadero", icon: "local_parking" },
   { path: "vehicles", title: "Vehículos", icon: "directions_car" },
   { path: "rates", title: "Tarifas", icon: "attach_money" }
 ];
