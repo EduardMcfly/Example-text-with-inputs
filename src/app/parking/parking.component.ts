6e48f53749e612388686babcd7ca6d00b3242f10
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2019-10-17 23:45:47 -0500

    Has been added dinamically form.

--- src/app/parking/parking.component.ts
+++ src/app/parking/parking.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, AbstractControl } from "@angular/forms";
 import * as _ from "lodash";
 
 interface Options {
@@ -13,6 +13,7 @@ interface Options {
 export class ParkingComponent implements OnInit {
   entryForm: FormGroup;
   submitted = false;
+  private separator: string = "#";
   /**
    *
    *
@@ -21,16 +22,38 @@ export class ParkingComponent implements OnInit {
    * @memberof ParkingComponent
    * @description `Here the options are defined in each form.`
    */
+  private text: string = `Lorem #ipsum, dolor sit amet consectetur adipisicing elit. 
+Ex, placeat labore, error #nisi provident, dolorum quasi consequatur veritatis quod in numquam
+voluptatibus a exercitationem! A reiciendis deserunt doloremque porro
+dolorem.`;
+
+  private textEnd: (string | { input: string })[] = [];
   private options: Options = { first: ["am", "am."] };
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.entryForm = this.formBuilder.group({
-      first: ""
-    });
+    this.initDynamicallyForm();
+  }
 
-    // get return url from route parameters or default to '/'
+  initDynamicallyForm() {
+    this.textEnd = [];
+    this.entryForm = this.formBuilder.group({});
+    let a: RegExpExecArray;
+    let text = this.text;
+    const execReg = value =>
+      new RegExp(`${this.separator}(\\S+)`, "g").exec(value);
+    while ((a = execReg(text))) {
+      const split = text.split(a[0]);
+      if (split.length > 2) {
+        console.log(`${a[0]}, debe ser unico`);
+      } else {
+        this.textEnd.push(split[0], { input: a[1] });
+        if (!execReg(split[1])) this.textEnd.push(split[1]);
+        this.entryForm.addControl(a[1], this.formBuilder.control(""));
+      }
+      text = split.join("");
+    }
   }
 
   // convenience getter for easy access to form fields
@@ -66,9 +89,10 @@ export class ParkingComponent implements OnInit {
       }
     }
 
-    if (this.entryForm.invalid) {
-      return;
-    }
-    console.log(entryForm);
+    console.log(this.entryForm);
+  }
+
+  isString(value: any) {
+    return typeof value === "string";
   }
 }
